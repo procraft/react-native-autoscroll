6ce4579317e73b66811deb07ce8b8121e0059b95
le-ar <andreiklauzer@gmail.com>
2024-01-17 13:39:50 +0300

    fix: package name

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -7,7 +7,7 @@ import {
   AutoScrollContext,
   AutoScrollContextRootProvider,
   AutoScrollScrollView,
-} from 'react-native-autoscroll';
+} from '@procraft/react-native-autoscroll';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function App() {
